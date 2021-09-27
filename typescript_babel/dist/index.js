d16310abc02077fa6f0a7d66241fc1383faeadc1
Isaac <Isaacgrimaldo432@gmail.com>
2021-09-27 02:58:36 -0500

    added the feature typescript-babel

---
+++ typescript_babel/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var sales = [200, 500, 700];
+var message = "Sales in junuary ".concat(sales[0]);
+var hello = 'df';
+var lola = 'Hello lola';
+console.log(message);
---
+++ typescript_babel/src/index.ts
@@ -0,0 +1,7 @@
+const sales:Number[] = [ 200 , 500 , 700 ]
+const message = `Sales in junuary ${sales[0]}`;
+const hello:string ='df';
+const lola='Hello lola'
+
+console.log(message);
+
